c3d3a445cf0fc000d275d8feca3127cd2834fa14
pablovisual <pabloarmandosandoval@gmail.com>
2023-02-15 04:27:47 +0000

    lab complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Object ref = new Object();
+        return ref;
     }
 }
